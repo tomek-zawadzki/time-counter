ef510ac8f8c144817407f7003eec448babb2fb30
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2022-11-13 17:43:53 +0100

    function to set and count time

--- app.js
+++ app.js
@@ -18,12 +18,34 @@ const eventSpan = document.querySelector(".event");
 
 let usersTime;
 
+const setTime = () => {
+  const currentTime = new Date();
+  const result = usersTime - currentTime;
+
+  const days = Math.floor(result / 1000 / 60 / 60 / 24);
+  const hours = Math.floor((result / 1000 / 60 / 60) % 24);
+  const minutes = Math.floor((result / 1000 / 60) % 60);
+  const seconds = Math.floor((result / 1000) % 60);
+
+  daysCount.textContent = days;
+  hoursCount.textContent = hours;
+  minutesCount.textContent = minutes;
+  secondsCount.textContent = seconds;
+};
+
 const appUpdate = () => {
   eventSpan.textContent = eventName.value;
+  usersTime = new Date(
+    `${eventMonth.value} ${eventDay.value} ${eventYear.value}`
+  );
   imageSection.style.backgroundImage = `url('${eventImage.value}')`;
+  setTime();
 };
 
 settingsBtn.addEventListener("click", () => {
   settings.classList.toggle("active");
 });
 saveBtn.addEventListener("click", appUpdate);
+
+appUpdate();
+setInterval(setTime, 1000);
